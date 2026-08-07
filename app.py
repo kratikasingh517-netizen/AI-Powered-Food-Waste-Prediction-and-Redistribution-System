e78fc43b712c7eb48703e8f23950d7bfed17bd49
kratikasingh517-netizen <kratikasingh517@gmail.com>
2026-08-07 10:35:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,29 +9,3 @@ if st.button("Predict"):
     st.write("📊 Predicted Waste: 120 kg")
     st.write("🤝 NGO Suggestions: Feeding India, Red Cross")
     st.write("⚡ Recommendation: Distribute within 4–6 hours")
-    ])
-
-    prediction = model.predict(input_data)
-
-    waste = round(float(prediction[0]),2)
-
-    st.success(
-        f"📊 Predicted Food Waste: {waste} kg"
-    )
-
-
-    if waste > 50:
-        st.warning(
-            "⚠️ High Waste Detected"
-        )
-        st.info(
-            "🤝 Suggested NGOs: Feeding India, Red Cross"
-        )
-        st.info(
-            "⚡ Redistribute within 4–6 hours"
-        )
-
-    else:
-        st.success(
-            "✅ Waste level is manageable"
-        )
